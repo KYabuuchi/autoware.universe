0e70f52df74dbfae0b53bd1e3766a77649f0b7cf
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-03-11 17:08:29 +0900

    add subscriber for pointcloud & pose
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/pose_estimator_manager/include/pose_estimator_manager/pose_estimator_manager.hpp
+++ localization/pose_estimator_manager/include/pose_estimator_manager/pose_estimator_manager.hpp
@@ -5,6 +5,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 #include <std_srvs/srv/set_bool.hpp>
 
 namespace multi_pose_estimator
@@ -13,15 +15,26 @@ class PoseEstimatorManager : public rclcpp::Node
 {
 public:
   using SetBool = std_srvs::srv::SetBool;
+  using PointCloud2 = sensor_msgs::msg::PointCloud2;
+  using PoseCovStamped = geometry_msgs::msg::PoseWithCovarianceStamped;
   PoseEstimatorManager();
 
 private:
+  rclcpp::Subscription<PoseCovStamped>::SharedPtr sub_pose_cov_;
+  rclcpp::Subscription<PointCloud2>::SharedPtr sub_map_;
   rclcpp::CallbackGroup::SharedPtr service_callback_group_;
   rclcpp::Service<SetBool>::SharedPtr switch_service_server_;
+  rclcpp::TimerBase::SharedPtr timer_;
 
   std::unordered_map<std::string, ManagerClient::SharedPtr> clients_;
+  std::optional<PoseCovStamped> latest_pose_{std::nullopt};
+
+  bool toggle_mode(bool enable_ndt);
 
   void on_service(SetBool::Request::ConstSharedPtr request, SetBool::Response::SharedPtr response);
+  void on_map(PointCloud2::ConstSharedPtr msg);
+  void on_timer();
+  void on_pose_cov(PoseCovStamped::ConstSharedPtr msg);
 };
 }  // namespace multi_pose_estimator
 
--- localization/pose_estimator_manager/src/pose_estimator_manager_core.cpp
+++ localization/pose_estimator_manager/src/pose_estimator_manager_core.cpp
@@ -1,11 +1,22 @@
 #include "pose_estimator_manager/pose_estimator_manager.hpp"
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 namespace multi_pose_estimator
 {
 PoseEstimatorManager::PoseEstimatorManager() : Node("pose_estimator_manager")
 {
   using std::placeholders::_1;
   using std::placeholders::_2;
+
+  const auto map_qos = rclcpp::QoS(1).transient_local().reliable();
+
+  auto on_map = std::bind(&PoseEstimatorManager::on_map, this, _1);
+  sub_map_ = create_subscription<PointCloud2>("/map/pointcloud", map_qos, on_map);
+  auto on_pose_cov = std::bind(&PoseEstimatorManager::on_pose_cov, this, _1);
+  sub_pose_cov_ =
+    create_subscription<PoseCovStamped>("/localization/pose_with_covariance", 10, on_pose_cov);
 
   // Service server
   auto on_service = std::bind(&PoseEstimatorManager::on_service, this, _1, _2);
@@ -18,21 +29,53 @@ PoseEstimatorManager::PoseEstimatorManager() : Node("pose_estimator_manager")
   clients_.emplace(
     "pcdless",
     std::make_shared<ManagerClient>(this, "/pcdless_enable_srv", service_callback_group_));
+
+  // Timer callback
+  auto on_timer = std::bind(&PoseEstimatorManager::on_timer, this);
+  timer_ =
+    rclcpp::create_timer(this, this->get_clock(), rclcpp::Rate(1).period(), std::move(on_timer));
+
+  toggle_mode(true);
 }
 
-void PoseEstimatorManager::on_service(
-  SetBool::Request::ConstSharedPtr request, SetBool::Response::SharedPtr response)
+bool PoseEstimatorManager::toggle_mode(bool enable_ndt)
 {
   RCLCPP_INFO_STREAM(get_logger(), "ENTER: toggle pose estimator");
-  if (request->data) {
+  if (enable_ndt) {
     clients_.at("ndt")->enable();
     clients_.at("pcdless")->disable();
   } else {
     clients_.at("ndt")->disable();
     clients_.at("pcdless")->enable();
   }
-  response->success = true;
   RCLCPP_INFO_STREAM(get_logger(), "EXIT: toggle pose estimator");
+  return true;
 }
+
+void PoseEstimatorManager::on_service(
+  SetBool::Request::ConstSharedPtr request, SetBool::Response::SharedPtr response)
+{
+  toggle_mode(request->data);
+  response->success = true;
+
+  RCLCPP_INFO_STREAM(get_logger(), "override mode and disable timer callback");
+  timer_->reset();
+}
+
+void PoseEstimatorManager::on_map(PointCloud2::ConstSharedPtr msg)
+{
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::fromROSMsg(*msg, cloud);
+
+  RCLCPP_INFO_STREAM(get_logger(), "on_map: PCD map initialization is done");
+}
+
+void PoseEstimatorManager::on_timer()
+{
+  RCLCPP_INFO_STREAM(get_logger(), "on_timer");
+  toggle_mode(true);
+}
+
+void PoseEstimatorManager::on_pose_cov(PoseCovStamped::ConstSharedPtr msg) { latest_pose_ = *msg; }
 
 }  // namespace multi_pose_estimator
