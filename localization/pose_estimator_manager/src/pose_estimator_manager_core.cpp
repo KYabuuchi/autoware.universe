b43e6535e7cf9b59c229bdfee46bb8e7f6932506
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-03-12 18:02:00 +0900

    switch pose_estimator automatically
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/pose_estimator_manager/src/pose_estimator_manager_core.cpp
+++ localization/pose_estimator_manager/src/pose_estimator_manager_core.cpp
@@ -2,6 +2,8 @@
 #include "pose_estimator_manager/pose_estimator_manager.hpp"
 
 #include <pcl_conversions/pcl_conversions.h>
+
+#include <sstream>
 namespace multi_pose_estimator
 {
 PoseEstimatorManager::PoseEstimatorManager() : Node("pose_estimator_manager")
@@ -11,8 +13,9 @@ PoseEstimatorManager::PoseEstimatorManager() : Node("pose_estimator_manager")
   const auto map_qos = rclcpp::QoS(1).transient_local().reliable();
 
   // Publisher
-  pub_marker_ =
-    create_publisher<MarkerArray>("/pcd_occupied_area", rclcpp::QoS(1).transient_local());
+  pub_grid_marker_ =
+    create_publisher<PointCloud2>("/pcd_occupied_area", rclcpp::QoS(1).transient_local());
+  pub_debug_string_ = create_publisher<String>("/debug/pcd_ratio_string", 10);
 
   // Subscriber
   auto on_map = std::bind(&PoseEstimatorManager::on_map, this, _1);
@@ -75,49 +78,56 @@ void PoseEstimatorManager::on_map(PointCloud2::ConstSharedPtr msg)
     grid_point_count[GridInfo(xyz.x, xyz.y)] += 1;
   }
 
-  occupied_areas_.clear();
+  occupied_areas_ = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
   for (const auto [grid, count] : grid_point_count) {
     if (count > 50) {
-      occupied_areas_.push_back(grid.get_center_point());
+      occupied_areas_->push_back(grid.get_center_point());
     }
   }
 
-  publish_occupied_area();
+  kdtree_ = pcl::make_shared<pcl::KdTreeFLANN<pcl::PointXYZ>>();
+  kdtree_->setInputCloud(occupied_areas_);
+
+  publish_occupied_area(msg->header);
   RCLCPP_INFO_STREAM(
-    get_logger(), "on_map: PCD map initialization is done " << occupied_areas_.size());
+    get_logger(), "on_map: PCD map initialization is done " << occupied_areas_->size());
 }
 
-void PoseEstimatorManager::publish_occupied_area()
+void PoseEstimatorManager::publish_occupied_area(const std_msgs::msg::Header & header)
 {
-  MarkerArray msg;
-  {
-    Marker marker;
-    marker.header.frame_id = "map";
-    marker.type = Marker::POINTS;
-    marker.color.r = 1.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0f;
-    marker.scale.x = 2.f;
-    marker.scale.y = 2.f;
-    marker.scale.z = 2.f;
-    marker.id = 0;
-    for (const auto xyz : occupied_areas_) {
-      geometry_msgs::msg::Point p;
-      p.x = xyz.x;
-      p.y = xyz.y;
-      p.z = xyz.z;
-      marker.points.push_back(p);
-    }
-    msg.markers.push_back(marker);
-  }
-  pub_marker_->publish(msg);
+  // Convert to msg
+  sensor_msgs::msg::PointCloud2 cloud_msg;
+  pcl::toROSMsg(*occupied_areas_, cloud_msg);
+  cloud_msg.header = header;
+  pub_grid_marker_->publish(cloud_msg);
 }
 
 void PoseEstimatorManager::on_timer()
 {
   RCLCPP_INFO_STREAM(get_logger(), "on_timer");
-  toggle_mode(true);
+
+  if (!latest_pose_.has_value()) {
+    RCLCPP_WARN_STREAM(
+      get_logger(), "Unable to determine which estimation to use, due to lack of latest position");
+    return;
+  }
+
+  auto position = latest_pose_->pose.pose.position;
+  pcl::PointXYZ query(position.x, position.y, position.z);
+
+  std::vector<int> indices;
+  std::vector<float> distances;
+  const int count = kdtree_->radiusSearch(query, 50, indices, distances, 0);
+  constexpr int COUNT_THR = 40;
+  toggle_mode(count > COUNT_THR);
+
+  {
+    String msg;
+    std::stringstream ss;
+    ss << "PCD occupancy: " << count << " > " << COUNT_THR;
+    msg.data = ss.str();
+    pub_debug_string_->publish(msg);
+  }
 }
 
 void PoseEstimatorManager::on_pose_cov(PoseCovStamped::ConstSharedPtr msg) { latest_pose_ = *msg; }
