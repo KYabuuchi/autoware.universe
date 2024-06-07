bfc75e6d8b181fda57c359c52e8b5e26fd7cc330
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2024-06-07 17:46:18 +0900

    replase autoware_auto_msg by autoware_msg
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/offline_nvtl_tool/README.md
+++ localization/offline_nvtl_tool/README.md
@@ -1,1 +1,31 @@
 # Offline NVTL Tool
+
+## How to use
+
+### 1 record rosbag
+
+```bash
+ros2 bag record --use-sim-time \
+    /map/pointcloud_map \
+    /localization/pose_estimator/pose_with_covariance \
+    /localization/util/downsample/pointcloud \
+    /tf \
+    /tf_static \
+    /perception/object_recognition/detection/centerpoint/objects \
+    /perception/object_recognition/detection/objects
+```
+
+### 2 launch offline_nvtl_tool
+
+```bash
+ros2 launch offline_nvtl offline_nvtl.launch.xml \
+  rosbag_path:=sample_rosbag
+```
+
+It creates `nvtl.csv` in the directory where launch.
+
+### 3 launch plot script
+
+```bash
+./install/offline_nvtl_tool/share/offline_nvtl_tool/scripts/plot_nvtl.py
+```
--- localization/offline_nvtl_tool/include/offline_nvtl_tool/offline_nvtl_tool.hpp
+++ localization/offline_nvtl_tool/include/offline_nvtl_tool/offline_nvtl_tool.hpp
@@ -3,7 +3,7 @@
 
 #include <rclcpp/node.hpp>
 
-#include <autoware_auto_perception_msgs/msg/detected_objects.hpp>
+#include <autoware_perception_msgs/msg/detected_objects.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
@@ -16,7 +16,7 @@
 class OfflineNvtlTool : public rclcpp::Node
 {
 public:
-  using DetectedObjects = autoware_auto_perception_msgs::msg::DetectedObjects;
+  using DetectedObjects = autoware_perception_msgs::msg::DetectedObjects;
   using PointCloud2 = sensor_msgs::msg::PointCloud2;
   using PoseCovStamped = geometry_msgs::msg::PoseWithCovarianceStamped;
   using Pose = geometry_msgs::msg::Pose;
