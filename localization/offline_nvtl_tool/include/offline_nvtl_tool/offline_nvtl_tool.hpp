496c091ac9595f0b1c665f0472bbf7439c6f1486
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2024-06-18 13:54:24 +0900

    use kinematic state
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/offline_nvtl_tool/include/offline_nvtl_tool/offline_nvtl_tool.hpp
+++ localization/offline_nvtl_tool/include/offline_nvtl_tool/offline_nvtl_tool.hpp
@@ -5,6 +5,7 @@
 
 #include <autoware_perception_msgs/msg/detected_objects.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 
@@ -25,6 +26,37 @@ public:
 };
 }  // namespace std
 
+struct MyTime
+{
+  int32_t sec;
+  uint32_t nanosec;
+  MyTime(const builtin_interfaces::msg::Time & time) : sec(time.sec), nanosec(time.nanosec) {}
+
+  bool operator<(const MyTime & other) const
+  {
+    if (sec == other.sec) {
+      return nanosec < other.nanosec;
+    }
+    return sec < other.sec;
+  }
+};
+using OdometryMap = std::map<MyTime, geometry_msgs::msg::Pose>;
+
+geometry_msgs::msg::Pose search_nearest_odometry(
+  const OdometryMap & odometry_map, const MyTime query)
+{
+  const auto it = odometry_map.lower_bound(query);
+  if (it == odometry_map.begin()) {
+    return it->second;
+  }
+  if (it == odometry_map.end()) {
+    return std::prev(it)->second;
+  }
+  // const auto prev_it = std::prev(it);
+  // prev_it->second is fine too
+  return it->second;
+}
+
 class OfflineNvtlTool : public rclcpp::Node
 {
 public:
@@ -34,6 +66,7 @@ public:
   using Pose = geometry_msgs::msg::Pose;
   using Marker = visualization_msgs::msg::Marker;
   using MarkerArray = visualization_msgs::msg::MarkerArray;
+  using Odometry = nav_msgs::msg::Odometry;
 
   OfflineNvtlTool();
 
@@ -65,6 +98,7 @@ private:
     std::vector<PointCloud2> point_cloud_vector;
     std::unordered_map<uint32_t, Pose> pose_map;
     std::unordered_map<uint32_t, DetectedObjects> objects_map;
+    OdometryMap odometry_map;
 
     // Extract all pose messages
     while (reader.has_next()) {
@@ -78,6 +112,10 @@ private:
         const PoseCovStamped pose_cov_stamped = decode_with_type<PoseCovStamped>(msg);
         pose_map.emplace(pose_cov_stamped.header.stamp.nanosec, pose_cov_stamped.pose.pose);
       }
+      if (msg->topic_name == "/localization/kinematic_state") {
+        const Odometry odometry = decode_with_type<Odometry>(msg);
+        odometry_map.emplace(odometry.header.stamp, odometry.pose.pose);
+      }
       if (msg->topic_name == "/localization/util/downsample/pointcloud") {
         const PointCloud2 point_cloud = decode_with_type<PointCloud2>(msg);
         point_cloud_vector.push_back(point_cloud);
@@ -89,15 +127,17 @@ private:
 
     for (const auto & point_cloud : point_cloud_vector) {
       const uint32_t nanosec = point_cloud.header.stamp.nanosec;
-      if (pose_map.find(nanosec) == pose_map.end()) {
-        continue;
-      }
+      // if (pose_map.find(nanosec) == pose_map.end()) {
+      //   continue;
+      // }
       if (objects_map.find(nanosec) == objects_map.end()) {
         continue;
       }
 
-      point_cloud_with_pose_array.push_back(
-        {point_cloud, pose_map.at(nanosec), objects_map.at(nanosec)});
+      const Pose odometry_pose =
+        search_nearest_odometry(odometry_map, MyTime{point_cloud.header.stamp});
+
+      point_cloud_with_pose_array.push_back({point_cloud, odometry_pose, objects_map.at(nanosec)});
     }
 
     RCLCPP_INFO_STREAM(
